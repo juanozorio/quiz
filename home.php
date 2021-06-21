614d1cb9e159c44b143c58b5f695a25448b48bb7
Técnico em Informática 2019.2 <39121742019.2@senacrjedu.local>
2021-06-21 20:32:52 -0300

    Alterações Home

--- home.php
+++ home.php
@@ -1,14 +1,42 @@
-<?php
-include 'verifica_login.php';
-?>
-<!DOCTYPE html>
+<!doctype html>
 <html lang="pt-br">
-<head>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="icon" href="../../../../favicon.ico">
 
-</head>
-<body>
-<h2>Olá,<?php echo $_SESSION['usuario'];?></h2>
+    <title>Template de capa, usando Bootstrap.</title>
 
-<h2><a href="logout.php">Sair</h2>
-</body>
+    <!-- Principal CSS do Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Estilos customizados para esse template -->
+    <link href="css/cover.css" rel="stylesheet">
+  </head>
+
+  <body class="text-center">
+
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+      <header class="masthead mb-auto">
+        <div class="inner">
+        </div>
+      </header>
+      <main role="main" class="inner cover">
+        <h1 class="cover-heading">Você é capaz de resolver o desafio ?</h1>
+        <p class="lead">O quiz é composto por 8 perguntas desafiadoras, será você capaz de responde-las corretamente? Seja atento e boa sorte!</p>
+        <p class="lead">
+          <a href="questao.php" class="btn btn-lg btn-secondary">Clique para começar o quiz!</a>
+        </p>
+      </main>
+      <footer class="mastfoot mt-auto">
+        <div class="inner">
+        <b>Projeto Quiz - 2021</b>
+        </div>
+      </footer>
+    </div>
+
+   
+  </body>
 </html>
